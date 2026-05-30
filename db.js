9cbb42893a7c0424bb1c6e611588d24fc61b07ab
Miiiishan <optmichan@gmail.com>
2026-05-30 13:23:58 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mishaeld0751",  // 👈 change this
-  database: "my_app"               // 👈 we will create this
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Mishaeld0751",
+  database: process.env.DB_NAME || "my_app",
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
+
   if (err) {
     console.log("❌ DB CONNECTION FAILED");
     console.log(err.message);
@@ -15,6 +17,7 @@ db.connect((err) => {
   }
 
   console.log("✅ DATABASE CONNECTED SUCCESSFULLY");
+
 });
 
 module.exports = db;
